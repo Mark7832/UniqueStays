6ae695b457e8a85c18caade10471d0640587a031
j4k0b <jakob.strmcnik@student.um.si>
2026-06-11 17:00:41 +0200

    fix: DB connection uses env variables

--- src/backend/ustvari_tabele.js
+++ src/backend/ustvari_tabele.js
@@ -1,11 +1,13 @@
 console.log('Skripta se je zagnala');
+require('dotenv').config();
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-    host: '127.0.0.1',
-    user: 'uniquestays',
-    password: 'geslo',
-    database: 'uniquestays'
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: parseInt(process.env.DB_PORT) || 3306,
+        user: process.env.DB_USER || 'uniquestays',
+        password: process.env.DB_PASSWORD || 'geslo',
+        database: process.env.DB_NAME || 'uniquestays'
     }
 });
 const bcrypt = require('bcryptjs');
